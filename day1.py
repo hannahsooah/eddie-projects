cdfb4c3e45d7eeec46280811ef63b1c691aa2947
Hannah Oh <0318sooah@gmail.com>
2021-02-25 18:01:28 -0800

    day1 end

--- day1.py
+++ day1.py
@@ -1,1 +1,56 @@
-print("Hello World")
+from operator import add, sub
+
+def a_plus_abs_b(a,b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> # a check that you didn't change the return statement!
+    >>> import inspect, re
+    >>> re.findall(r'^\s*(return .*)', inspect.getsource(a_plus_abs_b), re.M)
+    ['return f(a, b)']
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+def two_of_three(x, y, z):
+    """Return a*a + b*b, where a and b are the two smallest members of the
+    positive numbers x, y, and z.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    >>> # check that your code consists of nothing but an expression (this docstring)
+    >>> # a return statement
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
+    ['Expr', 'Return']
+    """
+
+    """
+    STEPS:
+    1. find the 2 smallest numbers
+        - conditional if's (to find smallest numbers)
+        if x < y:
+            # then something
+        else:
+            # then something else
+
+        if x < y:
+            # then something
+        elif:
+            # then something else
+    2. return a*a + b*b
+    """
+
+    return
