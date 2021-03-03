fc28f784cd859cc1f2205ecb1ccc3d02c962fb9b
Hannah Oh <0318sooah@gmail.com>
2021-03-02 18:05:30 -0800

    day 2

--- day1.py
+++ day1.py
@@ -1,5 +1,6 @@
 from operator import add, sub
 
+" Q2 "
 def a_plus_abs_b(a,b):
     """Return a+abs(b), but without calling abs.
 
@@ -18,6 +19,7 @@ def a_plus_abs_b(a,b):
         f = add
     return f(a, b)
 
+" Q3 "
 def two_of_three(x, y, z):
     """Return a*a + b*b, where a and b are the two smallest members of the
     positive numbers x, y, and z.
@@ -53,4 +55,89 @@ def two_of_three(x, y, z):
     2. return a*a + b*b
     """
 
+    "*** YOUR CODE HERE ***"
+
     return
+
+" Q4 "
+def largest_factor(n):
+    """ Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+" Q5 "
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result 
+
+def with_if_statement():
+    """
+    >>> result = with_if_statement()
+    47
+    >>> print(result)
+    None
+    """
+    if cond():
+        return true_func()
+    else:
+        return false_func()
+
+def with_if_function():
+    """
+    >>> result = with_if_function()
+    42
+    47
+    >>> print(result)
+    None
+    """
+    return if_function(cond(), true_func(), false_func())
+
+def cond():
+    "*** YOUR CODE HERE ***"
+
+def true_func():
+    "*** YOUR CODE HERE ***"
+
+def false_func():
+    "*** YOUR CODE HERE ***"
+
+
+" Q6 "
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
