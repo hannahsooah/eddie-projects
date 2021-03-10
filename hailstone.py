a2544ceae0c6cadd3fd09ea37b1f30a9e5c94cb9
Hannah Oh <0318sooah@gmail.com>
2021-03-09 18:54:50 -0800

    hailstone

---
+++ hailstone.py
@@ -0,0 +1,21 @@
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    1. pick a positive integer n as the start
+    2. if n is even, divide it by 2
+    3. if n is odd, multiply if by 3 and add 1
+    4. continue this process until n is 1
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
