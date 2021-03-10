dfe9f8cd912ad36748ee98c560412b7c2721d4dd
mango <jasontarshis.1@gmail.com>
2021-03-09 20:26:35 -0800

    s

--- hailstone.py
+++ hailstone.py
@@ -38,6 +38,6 @@ def hailstone(n):
         else:
             n = n * 3 + 1
             print(n)
-    if n = 1:
+    if n == 1:
         print("wow!!!")
-        return length 
+        return length
