5df97826c93942c5b0453f9847eeba9e7a51d1fa
mango <jasontarshis.1@gmail.com>
2021-03-16 18:56:27 -0700

    gamer omg pogchamp poggers

--- moreBasics.py
+++ moreBasics.py
@@ -15,13 +15,51 @@ def falling(n, k):
     1
     """
     "*** YOUR CODE HERE ***"
-    length == 1
+
+    """
+    6 * 5 * 4
+
+    n * (n-1) * (n-2) until we get to k arguments
+
+    we need to keep track of the length (number of arguments)
+
+     variable "product"
+
+    while loop: ends when length = k ==> while (length != k)
+    n = 6, k = 3
+    product = 6                                 length = 1
+    product = 6 * (5)                           length = 2
+    product = 6 * 5 * 4 = 30 * (4) = 120        length = 3
+
+    product = 4 = 4 * 1 = (n-length) * product  length = 1
+    product = 4 * 3 = product * (n-length)      length = 2
+    product = 4 * 3 * 2 = product * (n-length)  length = 3
+
+
+    n = 4
+    k = 3
+    product = 24
+    length = 2
+    """
+    product = 1
+    length = 0
+
+    while (length != k):
+        # whatever your loop is
+        product = product * (n-length)
+        length = length + 1
+    # whatever is outside your loop
+    return product
+
+    """length == 1
 
     while(k != 0):
-        k - 1
+        k = k - 1
         length = length + 1
+
     while(n != 0):
         (n * n) * length - 1
+    """
 
 
 "Q2"
@@ -38,12 +76,62 @@ def sum_digits(y):
     >>> a = sum_digits(123) # make sure that you are using return rather than print
     >>> a
     6
+    >>>  sum_digits(435)
+    12
     """
     "*** YOUR CODE HERE ***"
+
+    """
+    BRAINSTORM HERE
+
+    tips:
+    a % 10 returns last digit of a
+        234987 % 10 returns 7
+        32985 % 10 returns 5
+    a // 10 returns a without the last digit
+        234987 // 10 returns 23498
+        32985 // 10 returns 3298
+
+
+    a % b to make a last digit + sentence
+     use a // 10 to make a new last digit,
+     use a%b to make a last digit + sentence
+
+    sum_digits(4224)
+
+    answer = 12
+    y = 0
+    """
+
+    answer = 0
+
+    while (y > 0):
+        answer = answer + y % 10
+        y = y // 10
+
+    return answer
+
+
+
+    """
 
 "Q3"
 def cancel_culture(n, k):
     while (k != 0):
         number = k - 1
         n * number
-        print (number)
+        return (number)
+
+    doctests:
+
+    >>> cancel_culture(4, 3)
+
+
+
+
+
+    >>> cancel_culture(2, 1)
+
+    >>> cancel_culture(6, 2)
+
+    """
